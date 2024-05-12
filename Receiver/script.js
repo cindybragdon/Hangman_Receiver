f2d9663fba0d98cc45a5d98a4c88f5de1af91429
cindy <cindy.bragdon@gmail.com>
2024-05-12 18:24:36 -0400

    test one, test two

---
+++ Receiver/script.js
@@ -0,0 +1,72 @@
+const affichageMot = document.querySelector(".affichage-mot");
+const nbrEssais = document.querySelector(".nbr-essais b");
+const clavier = document.querySelector(".clavier");
+const cowboyImage = document.querySelector(".cowboy-image img");
+const divJeu = document.querySelector(".div-jeu");
+const rejouerBtn = divJeu.querySelector("bouton");
+
+// Déclaration des variables du jeu
+let motAdeviner, lettresOk, lettresPasOk;
+const essaisMax = 6;
+
+const initialiseJeu = () => {
+    lettresOk = [];
+    lettresPasOk = 0;
+    cowboyImage.src = "images/bonhomme-0.jpg";
+    nbrEssais.innerText = `${lettresPasOk} / ${essaisMax}`;
+    affichageMot.innerHTML = motAdeviner.split("").map(() => `<li class="lettre"></li>`).join("");
+    clavier.querySelectorAll("bouton").forEach(btn => btn.disabled = false);
+    divJeu.classList.remove("show");
+}
+
+const choisirMot = () => {
+    const { mot , indice, category } = listeDeMots[Math.floor(Math.random() * listeDeMots.length)];
+    motAdeviner = mot ;
+    document.querySelector(".indice-text b").innerText = indice;
+   document.querySelector(".categorie-text b").innerText = category;
+    initialiseJeu();
+}
+
+const gameOver = (isVictory) => {
+    // La petite fenêtre quand le jeu est terminé
+    const modalText = isVictory ? `Vous avez trouvé le mot:` : 'Le mot était:';
+    divJeu.querySelector("img").src = `images/${isVictory ? 'happyCowboy' : 'sadCowboy'}.jpg`;
+    divJeu.querySelector("h4").innerText = isVictory ? 'Petez-vous les bretelles! Vous êtes un champion!' : 'Ça vole pas haut votre affaire!';
+    divJeu.querySelector("p").innerHTML = `${modalText} <b>${motAdeviner}</b>`;
+    divJeu.classList.add("show");
+}
+
+const initGame = (bouton, clickedlettre) => {
+    // Checking if clickedlettre is exist on the motAdeviner
+    if(motAdeviner.includes(clickedlettre)) {
+        // Showing all correct lettres on the word display
+        [...motAdeviner].forEach((lettre, index) => {
+            if(lettre === clickedlettre) {
+                lettresOk.push(lettre);
+                affichageMot.querySelectorAll("li")[index].innerText = lettre;
+                affichageMot.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+    } else {
+        // If clicked lettre doesn't exist then update the lettresPasOk and hangman image
+        lettresPasOk++;
+        cowboyImage.src = `images/bonhomme-${lettresPasOk}.jpg`;
+    }
+    bouton.disabled = true; // Disabling the clicked bouton so user can't click again
+    nbrEssais.innerText = `${lettresPasOk} / ${essaisMax}`;
+
+    // Calling gameOver function if any of these condition meets
+    if(lettresPasOk === essaisMax) return gameOver(false);
+    if(lettresOk.length === motAdeviner.length) return gameOver(true);
+}
+
+// Creating clavier boutons and adding event listeners
+for (let i = 97; i <= 122; i++) {
+    const bouton = document.createElement("bouton");
+    bouton.innerText = String.fromCharCode(i);
+    clavier.appendChild(bouton);
+    bouton.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+}
+
+choisirMot();
+rejouerBtn.addEventListener("click", choisirMot);
---
+++ Receiver/word-list.js
@@ -0,0 +1,127 @@
+const listeDeMots = [
+    {
+        mot: "tabarnack",
+        indice: "meuble qui abrite le ciboire contenant les hosties consacrées au cours de la messe.",
+        category: "Sacrer comme un québécois"
+    },
+    {
+        mot: "champlure",
+        indice: "robinet.",
+        category: "Choses du quotidien"
+    },
+    {
+        mot: "guedille",
+        indice: "Pain à hot dog rempli d'une salade de homard.",
+        category: "Le manger"
+    },
+    {
+        mot: "germain",
+        indice: "Le masculin d'une femme qui gère  Une femme qui gère est une ________.",
+        category: "Prénom de boomer"
+    },
+    {
+        mot: "cloridorme ",
+        indice: "Ressemble à cloriforme.",
+        category: "Villes du Québec ou du Canada"
+    },
+    {
+        mot: "decalisser",
+        indice: "Partir très vite.",
+        category: "Sacrer comme un québécois"
+    },
+    {
+        mot: "garnotte",
+        indice: "Petites pierres.",
+        category: "Choses du quotidien"
+    },
+    {
+        mot: "gibelotte",
+        indice: "Ragout de poisson du bas du fleuve.",
+        category: "Le manger"
+    },
+    {
+        mot: "karen",
+        indice: "Je veux voir votre manager.",
+        category: "Prénom de boomer"
+    },
+    {
+        mot: "dildo",
+        indice: "Hey oui, il y a une ville du Canada qui a un nom vibrant.",
+        category: "Villes du Québec ou du Canada"
+    },
+    {
+        mot: "asteur",
+        indice: "Mot qui siginfie 'maintenant'.",
+        category: "Mots québécois"
+    },
+    {
+        mot: "gougounes",
+        indice: "Souliers ouvert pour l'été.",
+        category:"Mots québécois"
+    },
+    {
+        mot: "guenille",
+        indice: "Mot signifiant à la fois une étoffe pour nettoyer et des vêtements.",
+        category:"Mots québécois"
+    },
+    {
+        mot: "arsoude",
+        indice: "Qui veut dire arriver à l'improviste.",
+        category:"Mots québécois"
+    },
+    {
+        mot: "tiguidou",
+        indice: "Qui veut dire 'cest parfait, je suis d'accord' .",
+        category:"Mots québécois"
+    },
+    {
+        mot: "poutine",
+        indice: "Savant mélange de 3 ingrédients beaucoup trop salés dont Cindy rêve chaque instant de sa vie.",
+        category: "Le manger"
+    },
+    {
+        mot: "patechinois",
+        indice: "Savant mélange de 3 ingrédients beaucoup trop mous qui dégueule Cindy.",
+        category: "Le manger"
+    },
+    {
+        mot: "mayo",
+        indice: "Ville en Outaouais. ____ ketchup, moutarde.",
+        category: "Villes du Québec ou du Canada"
+    },
+    {
+        mot: "becosse",
+        indice: "Toilette extérieure.",
+        category:"Mots québécois"
+    },
+    {
+        mot: "bibitte",
+        indice: "Insecte.",
+        category:"Mots québécois"
+    },
+    {
+        mot: "pinotte",
+        indice: "Du beurre de.......",
+        category:"Le manger"
+    },
+    {
+        mot: "broche",
+        indice: "Attacher sa tuque avec dla ..... .",
+        category: "Expression québécoise"
+    },
+    {
+        mot: "buche",
+        indice: "Tire toé une...  .",
+        category: "Expression québécoise"
+    },
+    {
+        mot: "Jacques",
+        indice: "T'es habillé comme la chienne à ...  .",
+        category: "Expression québécoise"
+    },
+    {
+        mot: "embarque",
+        indice: "... dans mon char. (au Québec, on est les seuls a utiliser un verbe marin pour ça)",
+        category: "Expression québécoise"
+    }
+];
